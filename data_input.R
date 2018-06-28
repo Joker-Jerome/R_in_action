3c325b31fb2fe28ba90b5150d50c3225175e0d6d
Jerome Yu <jerome1993@qq.com>
2018-06-28 10:49:55 +0800

    update

---
+++ data_input.R
@@ -0,0 +1,15 @@
+# data input
+df <- data.frame(age = numeric(0), name = character(0), weight = numeric(0))
+edit(df)
+
+# read table
+# string will be converted to "factor" type
+# using stringsAsFactors=FALSE
+
+# read the file on the internet
+url()
+
+# read the excel file
+install.packages("RODBC")
+library(RODBC)
+
--- data_structures.R
+++ data_structures.R
@@ -9,3 +9,8 @@ b <- 1:3
 c <- c(1, "2", 3) # coerce
 list_a <- list(a_comp = a, b_comp = b, c_comp = c)
 list_a
+
+# object
+class(object)
+names(object)
+str(object)
