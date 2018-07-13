7ed2d3021dcd2ebb3489b35fccd0104d520f38d1
Jerome Yu <jerome1993@qq.com>
2018-07-13 07:22:40 -0700

    gc function

---
+++ memory.R
@@ -0,0 +1,5 @@
+# gc function
+gc()
+
+# Garbage Collection
+# A call of gc causes a garbage collection to take place. gcinfo sets a flag so that automatic collection is either silent (verbose = FALSE) or prints memory usage statistics (verbose = TRUE).
