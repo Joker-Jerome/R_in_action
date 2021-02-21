6f8ae550c8c311320781160e58cece15d1391cbe
Jerome Yu <jerome1993@qq.com>
2021-02-21 11:25:33 -0500

    variable_string

--- variable_string.R
+++ variable_string.R
@@ -7,10 +7,11 @@ myfunc(foo)
 [1] "foo"
 
 # from string to variable 
-
+# as.name() + eval()
 myFunc <- function(string) {
 	res = eval(as.name(paste0(string)))
 	return(res)
 }
 
-
+# get()
+get(string)
